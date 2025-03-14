8bb5ab6f7ae6651f02432405a0d28d2fb405c744
Dominik Kormann <alexander.kormann@outlook.de>
2025-03-14 15:46:47 +0100

    nothgin

--- index.md
+++ index.md
@@ -3,5 +3,4 @@
 ideas i sorta have:
 
 * [newpost](posts/newpost.md)
-* [bye](posts/bye.md)
 * [hello](posts/hello.md)
--- run.py
+++ run.py
@@ -2,10 +2,14 @@ import os
 
 os.listdir('posts')
 
+posts = os.listdir('posts')
+
+
 text = open("indextemplate.md").read().replace("{}",
   '\n'.join([
     f'* [{x[:-3]}](posts/{x})'
-    for x in os.listdir('posts') if x.endswith('.md')
+    for x in posts
+    if x.endswith('.md')
 ]))
 
 open("index.md", "w").write(text)
